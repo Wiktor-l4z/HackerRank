74baff9e694ef4318ac081e6907074c1a7fb8e28
Wiktor-l4z <wiktorkalinowski@gmail.com>
2016-08-11 22:03:11 +0200

    Solutions

---
+++ src/hackerrank/JavaStdinAndStdoutI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author l4z
+ */
+public class JavaStdinAndStdoutI {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
