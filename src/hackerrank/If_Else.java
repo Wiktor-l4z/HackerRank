9d6e6abd67642f60ae99b4d19ca49a7c0ae3da0d
Wiktor-l4z <wiktorkalinowski@gmail.com>
2016-08-11 21:48:24 +0200

    IF_ELSE

---
+++ src/hackerrank/If_Else.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author l4z
+ */
+public class If_Else {
+    
+  public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();            
+            String ans = "";
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else if (2 <= n && n <= 5) {
+                 ans = "Not Weird";
+            }          
+            else if (6 <= n && n <= 20){
+                 ans = "Weird";
+            }          
+           else {
+                 ans = "Not Weird";
+            }          
+            
+                  System.out.println(ans);
+            
+        }
+    
+    
+    
+}
