a4817d59b877697e2746d68d0e6ba21d04cba07e
Wiktor <wiktorkalinowski@gmail.com>
2016-12-11 23:43:03 +0100

    Day 28 of Code!

---
+++ src/30 Days of Code Challenges/Day28/User.java
@@ -0,0 +1,30 @@
+package binarysearchtree;
+
+
+
+        import java.util.HashSet;
+        import java.util.Set;
+
+public class User {
+
+    String username;
+    String password;
+    int age;
+    Set<Integer> orderIDs;
+
+
+    public User(String customUserName, String customPassword, int customAge, Set<Integer> orderIDs) {
+        this.username = customUserName;
+        this.password = customPassword;
+        this.age = customAge;
+        this.orderIDs = orderIDs;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Set a = new HashSet();
+        a.add(1212);
+        User kathryn = new User("blondiebytes", "hello world", 10, a);
+    }
+
+}
